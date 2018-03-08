923ef76f9d80e754b63460ee54378a44d4e52b10
jameson141 <jamesonmiyashiro@gmail.com>
2018-03-07 14:35:12 -1000

    final commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var colorsArray = ["red", "white", "black", "yellow"];
+colors.splice()
+var thingsObject = {
+	Books : {
+		good : "that one"
+
+	}
+	Movies : {
+	    bad : "Emoji"
+
+    }
+    Shows : {
+    	great : "terrible"
+    }
+
+}
+   
